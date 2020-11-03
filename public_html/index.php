77378be764ca0c553dfb33b36334d49c4cd1b02e
Oscar <otg0203132gmail.com>
2020-11-03 19:51:48 +0100

    Added basic page HTML to file

--- public_html/index.php
+++ public_html/index.php
@@ -5,3 +5,16 @@ $pages_path = "../private_html/pages";
 $vendor_path = "../private_html/vendor";
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
