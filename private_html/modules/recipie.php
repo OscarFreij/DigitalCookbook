6023cd49ffd462021a2aebfa4c160b0990ff56b9
Oscar Freij <otg020313@gmail.com>
2020-11-26 01:05:47 +0100

    added 1 new module and debug messages for the all viable modules

--- private_html/modules/recipie.php
+++ private_html/modules/recipie.php
@@ -1,3 +1,5 @@
 <div>
-    <p>Basic reipie module :)</p>
+    <?php
+    echo("<h1>Loaded module-sub \"recipie.php\"</h1>")
+    ?>
 </div>
---
+++ private_html/modules/search.php
@@ -0,0 +1,5 @@
+<div>
+    <?php
+    echo("<h1>Loaded module-sub \"search.php\"</h1>")
+    ?>
+</div>
