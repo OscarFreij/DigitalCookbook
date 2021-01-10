aee61b4c0a564b441fcb249b70834a5b30036b6d
Oscar Freij <otg020313@gmail.com>
2021-01-10 03:05:12 +0100

    More work done on UI and button toggel for the cookbook page.

--- private_html/pages/cookbook.php
+++ private_html/pages/cookbook.php
@@ -1,10 +1,9 @@
-<div>
-    <?php
-    echo("<h1>Loaded module-page \"cookbook.php\"</h1>")
-    ?>
-    <div id="category_wrapper">
-        <div id="category_0" class="category" data-order="0"><button onClick="moveCategoryUp(0)">/\</button><button onClick="moveCategoryDown(0)">\/</button><p>Alpha</p></div>
-        <div id="category_1" class="category" data-order="1"><button onClick="moveCategoryUp(1)">/\</button><button onClick="moveCategoryDown(1)">\/</button><p>Bravo</p></div>
-        <div id="category_2" class="category" data-order="2"><button onClick="moveCategoryUp(2)">/\</button><button onClick="moveCategoryDown(2)">\/</button><p>Charlie</p></div>
-    </div>
+<div id="category_wrapper">
+    <div id="category_0" class="category" data-order="0"><p>Alpha</p></div>
+    <div id="category_1" class="category" data-order="1"><p>Bravo</p></div>
+    <div id="category_2" class="category" data-order="2"><p>Charlie</p></div>
 </div>
+<div id="content_wrapper">
+
+</div>
+<button onClick="ToggleEdit()">Toggle Edit</button>
--- public_html/static/js/cookbook.js
+++ public_html/static/js/cookbook.js
@@ -1,3 +1,6 @@
+var editingMode = false;
+
+
 function moveCategoryUp(id)
 {
     if (id >= 0)
@@ -13,10 +16,10 @@ function moveCategoryUp(id)
             element1.id = "category_"+element1.dataset.order;
             element2.id = "category_"+element2.dataset.order;
     
-            element1.children[0].setAttribute( "onClick", "moveCategoryUp("+element1.dataset.order+")");
-            element2.children[0].setAttribute( "onClick", "moveCategoryUp("+element2.dataset.order+")");
-            element1.children[1].setAttribute( "onClick", "moveCategoryDown("+element1.dataset.order+")");
-            element2.children[1].setAttribute( "onClick", "moveCategoryDown("+element2.dataset.order+")");
+            element1.querySelector(".EditPanel").children[0].setAttribute( "onClick", "moveCategoryUp("+element1.dataset.order+")");
+            element2.querySelector(".EditPanel").children[0].setAttribute( "onClick", "moveCategoryUp("+element2.dataset.order+")");
+            element1.querySelector(".EditPanel").children[1].setAttribute( "onClick", "moveCategoryDown("+element1.dataset.order+")");
+            element2.querySelector(".EditPanel").children[1].setAttribute( "onClick", "moveCategoryDown("+element2.dataset.order+")");
         }
         reorderCategories();
     }
@@ -37,16 +40,68 @@ function moveCategoryDown(id)
             element1.id = "category_"+element1.dataset.order;
             element2.id = "category_"+element2.dataset.order;
     
-            element1.children[0].setAttribute( "onClick", "moveCategoryUp("+element1.dataset.order+")");
-            element2.children[0].setAttribute( "onClick", "moveCategoryUp("+element2.dataset.order+")");
-            element1.children[1].setAttribute( "onClick", "moveCategoryDown("+element1.dataset.order+")");
-            element2.children[1].setAttribute( "onClick", "moveCategoryDown("+element2.dataset.order+")");
+            element1.querySelector(".EditPanel").children[0].setAttribute( "onClick", "moveCategoryUp("+element1.dataset.order+")");
+            element2.querySelector(".EditPanel").children[0].setAttribute( "onClick", "moveCategoryUp("+element2.dataset.order+")");
+            element1.querySelector(".EditPanel").children[1].setAttribute( "onClick", "moveCategoryDown("+element1.dataset.order+")");
+            element2.querySelector(".EditPanel").children[1].setAttribute( "onClick", "moveCategoryDown("+element2.dataset.order+")");
         }
         reorderCategories();
     }
 }
 
+function addEditBtn(row)
+{
+    element1 = document.createElement("div");
+    element1.classList.add("EditPanel");
+    element1.classList.add("btn-group");
 
+    btn1 = document.createElement("button");
+    btn2 = document.createElement("button");
+
+    btn1.setAttribute( "onClick", "moveCategoryUp("+row.dataset.order+")");
+    btn1.appendChild(document.createTextNode("UP"));
+    btn1.classList.add("btn");
+    btn1.classList.add("btn-primary");
+
+    btn2.setAttribute( "onClick", "moveCategoryDown("+row.dataset.order+")");
+    btn2.appendChild(document.createTextNode("DOWN"));
+    btn2.classList.add("btn");
+    btn2.classList.add("btn-primary");
+
+    element1.appendChild(btn1);
+    element1.appendChild(btn2);
+
+    row.appendChild(element1);
+}
+
+function removeEditBtn(row)
+{
+    row.getElementsByClassName("EditPanel")[0].remove();
+}
+
+
+function ToggleEdit()
+{
+    var $categories = $('#category_wrapper').find('.category');
+
+    if (!editingMode)
+    {
+        for (let i = 0; i < $categories.length; i++) {
+            const element = $categories[i];
+            addEditBtn(element);
+        }
+        editingMode = true;    
+    }
+    else
+    {
+        for (let i = 0; i < $categories.length; i++) {
+            const element = $categories[i];
+            removeEditBtn(element);
+        }
+        editingMode = false;    
+    }
+    
+}
 
 
 function reorderCategories()
